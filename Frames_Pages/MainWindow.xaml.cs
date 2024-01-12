9fb820b3fa198e49868ca5d9b69d44a3c6fd0e99
2324-Ghise-Claudiu-BSZ <ghisevasileclaudiu@bs-intra.de>
2024-01-12 10:02:50 +0100

    Added ViewBox to be able to scale the window and its contents properly. Added a Screensize property to be able to determine the size of the screen on witch the app will run.

--- Frames_Pages/MainWindow.xaml.cs
+++ Frames_Pages/MainWindow.xaml.cs
@@ -20,13 +20,28 @@ namespace Frames_Pages
     /// </summary>
     public partial class MainWindow : Window
     {
+        double screenWidth = SystemParameters.PrimaryScreenWidth ;
+        double screenHeight = SystemParameters.PrimaryScreenHeight - 100;
+
         public MainWindow()
         {
             InitializeComponent();
             MainPage mainPage = new MainPage();
             RegPage regPage = new RegPage();
-            
 
+            // Set up data binding for Width
+            SetBinding(WidthProperty, new System.Windows.Data.Binding
+            {
+                Source = screenWidth,
+                Mode = System.Windows.Data.BindingMode.OneWay
+            });
+
+            // Set up data binding for Height
+            SetBinding(HeightProperty, new System.Windows.Data.Binding
+            {
+                Source = screenHeight,
+                Mode = System.Windows.Data.BindingMode.OneWay
+            });
             frame.NavigationService.Navigate(new Uri("mainPage.xaml", UriKind.Relative));
         }
 
